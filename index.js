08cbe7316dd8af30c820ab35765a5d96e6fbb024
drezix <andre.t.agostinis@gmail.com>
2025-03-18 14:25:57 -0300

    feat(main): adicionar rota para testar processos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const connectDB = require('./src/config/database');
 const clientRoutes = require('./src/routes/clientRoutes');
 const lawyerRoutes = require('./src/routes/lawyerRoutes');
+const processRoutes = require('./src/routes/processRoutes');
 const express = require('express')
 
 const app = express()
@@ -10,8 +11,9 @@ connectDB();
 
 app.use(express.json());
 
-app.use('/user', clientRoutes);
+app.use('/client', clientRoutes);
 app.use('/lawyer', lawyerRoutes);
+app.use('/process', processRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
