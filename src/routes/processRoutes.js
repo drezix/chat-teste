76edf8ee5286dfc01eae8e9fef65b3ff1987d305
drezix <andre.t.agostinis@gmail.com>
2025-03-18 13:08:13 -0300

    feat(process): adicionar rotas dos processos

---
+++ src/routes/processRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const processController = require('../controllers/processController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const {lawyerMiddleware} = require('../middlewares/lawyerMiddleware');
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.post('/insert', lawyerMiddleware, processController.insert);
+router.get('/get/:id',processController.get);
+router.delete('/delete/:id',lawyerMiddleware, processController.delete);
+
+module.exports = router;
